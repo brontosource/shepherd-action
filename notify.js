ceebe038aa44c2ac97e31164d45fd34b38ddc45c
Eric Li <li.zhe.hua@gmail.com>
2026-02-05 12:13:55 -0500

    Add workflow filename to payload

--- notify.js
+++ notify.js
@@ -1,5 +1,16 @@
 import * as core from "@actions/core";
 
+function parseWorkflowFilename(workflowRef) {
+  const WORKFLOW_DIRECTORY = '/.github/workflows/';
+  const dirStart = workflowRef.indexOf(WORKFLOW_DIRECTORY);
+  if (dirStart === -1) { return null; }
+  const filenameStart = dirStart + WORKFLOW_DIRECTORY.length;
+  let refStart = workflowRef.indexOf('@refs/', filenameStart);
+  if (refStart === -1) { refStart = workflowRef.lastIndexOf('@'); }
+  if (refStart === -1) { return null; }
+  return workflowRef.substring(filenameStart, refStart);
+}
+
 export async function notify(isPre) {
   const token = process.env['INPUT_BRONTO-SHEPHERD-WORKFLOW-TOKEN'];
   const url = process.env['INPUT_BRONTO-SHEPHERD-URL'];
@@ -13,6 +24,13 @@ export async function notify(isPre) {
     return;
   }
 
+  const workflowRef = process.env.GITHUB_WORKFLOW_REF;
+  const workflow = parseWorkflowFilename(workflowRef);
+
+  if (!workflow) {
+    core.setFailed(`Failed to parse workflow ref '${workflowRef}'`);
+    return;
+  }
 
   const [owner, name] = process.env.GITHUB_REPOSITORY.split('/');
   const response = await fetch(url, {
@@ -23,6 +41,7 @@ export async function notify(isPre) {
     body: JSON.stringify({
       token,
       start: isPre,
+      workflow,
       run_id: process.env.GITHUB_RUN_ID,
       repo_owner: owner,
       repo_name: name
